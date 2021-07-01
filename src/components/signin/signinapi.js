420c98800831516962dba1cfa0af61212f4d509a
Justin Elmore <jelmore1674@pm.me>
2021-07-01 18:47:35 -0500

    updated readme, and removed unused files.

--- src/components/signin/signinapi.js
+++
@@ -1,9 +0,0 @@
-export function getSignin(item) {
-    return fetch('https://damp-depths-04548.herokuapp.com/signin', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ item }),
-    }).then((data) => data.json());
-}
